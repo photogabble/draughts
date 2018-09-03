6833df912ce5f8e78eeae7045a77e20dc6760d97
Simon Dann <carbontwelve@users.noreply.github.com>
2018-09-03 23:43:58 +0100

    Ported some more of the tests

--- tests/DraughtsTest.php
+++ tests/DraughtsTest.php
@@ -10,26 +10,75 @@ class DraughtsTest extends TestCase
 
     public function perftTest()
     {
-        $draughts = new Draughts();
-        // @todo
+        $perfts = [];
+
+        foreach ($perfts as $perft) {
+            $draughts = new Draughts();
+            $draughts->load($perft['fen']);
+            $nodes = $draughts->perft($perft['depth']);
+            $this->assertEquals($perft['nodes'], $nodes);
+        }
     }
 
     public function singleSquareMoveGenerationTest()
     {
-        $draughts = new Draughts();
-        // @todo
+        $positions = [];
+
+        foreach ($positions as $position) {
+            $draughts = new Draughts();
+            $draughts->load($position['fen']);
+
+
+            $moves = $draughts->moves(['square' => $position['square'], 'verbose' =>  $position['verbose']]);
+            $passed = count($position['moves']) === count($moves);
+
+            for ($j = 0; $j < count($moves); $j++) {
+                if (!$position['verbose']) {
+                    $passed = $passed && $moves[$j] == $position['moves'][$j];
+                } else {
+                    foreach ($moves[$j] as $k) {
+                        $passed = $passed && $moves[$j][$k] == $position['moves'][$j][$k];
+                    }
+                }
+            }
+            $this->assertTrue($passed);
+        }
     }
 
     public function insufficientMaterialTest()
     {
-        $draughts = new Draughts();
-        // @todo
+        $positions = [];
+
+        foreach ($positions as $position) {
+            $draughts = new Draughts();
+            $draughts->load($position['fen']);
+
+            if ($position['draw']) {
+                $this->assertTrue($draughts->insufficientMaterial() && $draughts->inDraw());
+            } else {
+                $this->assertTrue(!$draughts->insufficientMaterial() && !$draughts->inDraw());
+            }
+        }
     }
 
     public function threefoldRepetitionTest()
     {
-        $draughts = new Draughts();
-        // @todo
+        $positions = [];
+
+        foreach ($positions as $position) {
+            $draughts = new Draughts();
+            $draughts->load($position['fen']);
+            $passed = true;
+            for ($j = 0; $j < count($position['moves']); $j++) {
+                if ($draughts->InThreefoldRepetition()) {
+                    $passed = false;
+                    break;
+                }
+                $draughts->move($position['moves'][$j]);
+            }
+
+            $this->assertTrue($passed && $draughts->inThreefoldRepetition() && $draughts->inDraw());
+        }
     }
 
     public function getPutRemoveTest()
@@ -40,8 +89,13 @@ class DraughtsTest extends TestCase
 
     public function FENTest()
     {
-        $draughts = new Draughts();
-        // @todo
+        $positions = [];
+
+        foreach ($positions as $position) {
+            $draughts = new Draughts();
+            $draughts->load($position['fen']);
+            $this->assertEquals($position['should_pass'], $draughts->fen == $position['fen']);
+        }
     }
 
     public function loadFENTest()
@@ -52,14 +106,14 @@ class DraughtsTest extends TestCase
 
     public function makeMoveTest()
     {
-        $draughts = new Draughts();
-        // @todo
-    }
+        $positions = [];
 
-    public function validateFENTest()
-    {
-        $draughts = new Draughts();
-        // @todo
+        foreach ($positions as $position) {
+            $draughts = new Draughts();
+            $draughts->load($position['fen']);
+
+            // @todo
+        }
     }
 
     public function historyTest()
